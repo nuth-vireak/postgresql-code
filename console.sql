374da293492d39eff29e8365f01a44f6db58c75f
nuth-vireak <timberr.4t@gmail.com>
2023-02-25 00:25:22 +0700

    init

---
+++ console.sql
@@ -0,0 +1,11 @@
+-- Create a table called person
+CREATE TABLE person (
+    id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(10),
+    date_of_birth DATE
+);
+
+-- Select all columns from the person table
+SELECT * FROM person;
